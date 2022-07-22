5147771ae57492e5994dc04efd77d36bccb1396a
Beatriz Janet Ibarra Estrada <bibarra@arteli.com.mx>
2022-07-22 12:01:44 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RestService } from './rest.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'test';
+  public listadetitulos:any = []
+
+  constructor(private RestService:RestService)
+  {
+
+  }
+
+  ngOnInit(): void {
+    this.cargaData()
+  }
+
+  public cargaData(){
+    this.RestService.get('https://api.github.com/search/repositories?q=php+language:php')
+    .subscribe(respuesta =>{
+      console.log(respuesta);
+      //this.listadetitulos = respuesta; 
+    }
+      )
+  }
 }
